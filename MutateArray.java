b855582d04298a9f49b007ba08cc15f18a0bef1c
Joyce Silvera <60859729+joycesilvera@users.noreply.github.com>
2021-08-15 14:48:27 -0700

    Create MutateArray.java

---
+++ MutateArray.java
@@ -0,0 +1,18 @@
+class MutateArray{
+
+int[] mutateTheArray(int n, int[] a) {
+    int [] b = new int[a.length];
+    
+    for(int I = 0; I < a.length; I++){
+        int x = i-1 < 0 ? 0 : a[i-1];
+        int y = a[I];
+        int z = I+1 >= a.length ? 0 : a[I+1];
+        
+        b[I] = x + y+ z;
+    }
+    
+    return b;
+}
+
+
+}
