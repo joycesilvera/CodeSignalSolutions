35e522239e7bd7c60f55585f2f9087d55b17040b
Joyce Silvera <60859729+joycesilvera@users.noreply.github.com>
2021-08-17 10:59:33 -0700

    Update AlternatingSort.java

--- AlternatingSort.java
+++ AlternatingSort.java
@@ -2,37 +2,23 @@ boolean alternatingSort(int[] a) {
     int l = 0, r = a.length-1, index = 0;
     int [] b = new int[a.length];
     
-    if(a.length % 2 == 0){
-            while(l < r){
+    
+    while(l < r){
         int x = a[l++];
         int y = a[r—];
         
-
-            
         b[index++] = x;
         b[index++] = y;
         
     }
-    }
-    else{
-        while(l < r){
-        int x = a[l++];
-        int y = a[r—];
-        
-
-            
-        b[index++] = x;
-        b[index++] = y;
-        
-       }
+   
+    if(a.length % 2 != 0){
        b[index++] = a[l++];
-    }
+     }
     
 
-   // System.out.println(b[0]);
-    for(int I = 1;  I < b.length; I++){
-       // System.out.println(b[I]);
-        if( b[I] <= b[i-1])
+    for(int i = 1;  i < b.length; i++){
+        if( b[i] <= b[i-1])
             return false;
     }
         
